e08cd3b67a19199cbaab20844c46266a8136a645
kouqhar <naphtaliduniya2@gmail.com>
2023-04-12 12:55:50 +0100

    NPM PACKAGE : New Feature, App-to-user payment added and dependencies update

--- example/example.js
+++ example/example.js
@@ -17,7 +17,7 @@ piecard
   .then((response) => {
     console.log("Returned Payment : ", response);
   })
-  .catch((err) => console.log("Geting payment error : ", err));
+  .catch((err) => console.log("Getting payment error : ", err));
 
 // CREATE PAYMENT
 const paymentData = {
@@ -34,6 +34,21 @@ piecard
     console.log("Created Payment : ", response);
   })
   .catch((err) => console.log("Creating payment error : ", err));
+
+// SEND PI FROM APP TO USER (APP-TO-USER TRANSACTION)
+const appToUserData = {
+  amount: NUMBER, // Will be deducted from your wallet, plus the 0.02 Pi fee
+  recipient: STRING, // Either Pi Username OR Wallet Address
+  wallet_public_key: STRING, // Your Mainnet wallet address
+  wallet_secret_key: STRING, // We will never see or store this
+  memo: STRING, // Optional - this will appear on the blockchain
+};
+piecard
+  .makePaymentToUser(appToUserData)
+  .then((response) => {
+    console.log("Pi Payment response : ", response);
+  })
+  .catch((err) => console.log("Sending pi to user error : ", err));
 
 // GET A SECURE PAYMENT PAYLOAD
 const encryptedData = STRING; // See "./keys_sample" for example data
